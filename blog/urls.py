9469031cbe586e6f2e03ccb4230776a4dcdf4c71
Kexin Feng <fkx.333@gmail.com>
2016-03-13 23:36:19 -0700

    Added view and template for detailed blog post as well as CSS

--- blog/urls.py
+++ blog/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
 	# name each URL and be unique
 
 	url(r'^$', views.post_list, name = 'post_list'),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 # The dot before models means current directory or current application
 # both views.py and models.py are in the same directory. This means we can use . and the name of the file (without .py)
@@ -11,3 +11,7 @@ def post_list(request):
 	# request: a parameter, everything we receive from the user via the Internet 
 	# the last parameter {}, is a place in which we can add some things for the template to use
 	return render(request, 'blog/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request,'blog/post_detail.html', {'post': post})
